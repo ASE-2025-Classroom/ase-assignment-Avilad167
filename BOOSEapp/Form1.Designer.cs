1a578cc3e18112df00d1caf88dc7e95b031f996f
avi lad <A.Lad2535@student.leedsbeckett.ac.uk>
2025-10-07 13:01:54 +0100

    done basic examples 1

--- BOOSEapp/Form1.Designer.cs
+++ BOOSEapp/Form1.Designer.cs
@@ -2,38 +2,79 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxProgram;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.PictureBox pictureBoxCanvas;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.textBoxProgram = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.pictureBoxCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxProgram
+            // 
+            this.textBoxProgram.Location = new System.Drawing.Point(12, 12);
+            this.textBoxProgram.Multiline = true;
+            this.textBoxProgram.Name = "textBoxProgram";
+            this.textBoxProgram.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxProgram.Size = new System.Drawing.Size(350, 400);
+            this.textBoxProgram.TabIndex = 0;
+            this.textBoxProgram.Text = "moveto 100,100\r\npen 0,255,0\r\ncircle 50\r\npen 255,0,0\r\nmoveto 150,50\r\nrect 50,100";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 420);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(350, 100);
+            this.textBoxOutput.TabIndex = 1;
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(12, 530);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(350, 30);
+            this.buttonRun.TabIndex = 2;
+            this.buttonRun.Text = "Run BOOSE Program";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // pictureBoxCanvas
+            // 
+            this.pictureBoxCanvas.BackColor = System.Drawing.Color.White;
+            this.pictureBoxCanvas.Location = new System.Drawing.Point(370, 12);
+            this.pictureBoxCanvas.Name = "pictureBoxCanvas";
+            this.pictureBoxCanvas.Size = new System.Drawing.Size(600, 550);
+            this.pictureBoxCanvas.TabIndex = 3;
+            this.pictureBoxCanvas.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(984, 571);
+            this.Controls.Add(this.pictureBoxCanvas);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxProgram);
+            this.Name = "Form1";
+            this.Text = "BOOSE Interpreter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
-
-        #endregion
     }
 }
--- BOOSEapp/Form1.cs
+++ BOOSEapp/Form1.cs
@@ -1,13 +1,158 @@
 using BOOSE;
+using System;
 using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace BOOSEapp
 {
     public partial class Form1 : Form
     {
+        private Point currentPos = new Point(0, 0);
+        private Pen currentPen = new Pen(Color.Black, 2);
+        private Bitmap canvasBitmap;
+
         public Form1()
         {
             InitializeComponent();
-            Debug.WriteLine(AboutBOOSE.about());
+
+            // Create bitmap canvas and assign to picture box
+            canvasBitmap = new Bitmap(pictureBoxCanvas.Width, pictureBoxCanvas.Height);
+            pictureBoxCanvas.Image = canvasBitmap;
+
+            // Call About method and display
+            string aboutText = AboutBOOSE.about();
+            textBoxOutput.AppendText("About BOOSE:\r\n" + aboutText + "\r\n");
+            Debug.WriteLine("About BOOSE:");
+            Debug.WriteLine(aboutText);
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            // Clear the canvas
+            using (Graphics g = Graphics.FromImage(canvasBitmap))
+            {
+                g.Clear(Color.White);
+            }
+            pictureBoxCanvas.Invalidate();
+
+            // Reset position and pen
+            currentPos = new Point(0, 0);
+            currentPen = new Pen(Color.Black, 2);
+
+            // Clear output
+            textBoxOutput.Clear();
+            textBoxOutput.AppendText("Running BOOSE program...\r\n");
+
+            // Run program line by line
+            string[] lines = textBoxProgram.Text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            using (Graphics g = Graphics.FromImage(canvasBitmap))
+            {
+                foreach (var line in lines)
+                {
+                    try
+                    {
+                        ExecuteCommand(line.Trim(), g);
+                        Debug.WriteLine("Executed: " + line.Trim());
+                    }
+                    catch (Exception ex)
+                    {
+                        string error = $"Error executing '{line}': {ex.Message}";
+                        textBoxOutput.AppendText(error + "\r\n");
+                        Debug.WriteLine(error);
+                    }
+                }
+            }
+
+            pictureBoxCanvas.Invalidate();
+            textBoxOutput.AppendText("Program finished.\r\n");
+        }
+
+        private void ExecuteCommand(string line, Graphics g)
+        {
+            if (string.IsNullOrWhiteSpace(line)) return;
+            if (line.StartsWith("*")) return; 
+
+            string[] parts = line.Split(' ', 2);
+            if (parts.Length < 2) return;
+
+            string cmd = parts[0].ToLower();
+            string args = parts[1];
+
+            switch (cmd)
+            {
+                case "moveto":
+                    {
+                        var coords = args.Split(',');
+                        if (coords.Length != 2) throw new ArgumentException("Invalid moveto arguments");
+                        if (int.TryParse(coords[0], out int x) && int.TryParse(coords[1], out int y))
+                        {
+                            currentPos = new Point(x, y);
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Invalid moveto coordinates");
+                        }
+                        break;
+                    }
+                case "pen":
+                    {
+                        var colors = args.Split(',');
+                        if (colors.Length != 3) throw new ArgumentException("Invalid pen color arguments");
+                        if (int.TryParse(colors[0], out int r) && int.TryParse(colors[1], out int gCol) && int.TryParse(colors[2], out int b))
+                        {
+                            currentPen.Color = Color.FromArgb(r, gCol, b);
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Invalid pen color values");
+                        }
+                        break;
+                    }
+                case "circle":
+                    {
+                        if (int.TryParse(args, out int radius))
+                        {
+                            Rectangle rect = new Rectangle(currentPos.X - radius, currentPos.Y - radius, radius * 2, radius * 2);
+                            g.DrawEllipse(currentPen, rect);
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Invalid circle radius");
+                        }
+                        break;
+                    }
+                case "rect":
+                    {
+                        var sizes = args.Split(',');
+                        if (sizes.Length != 2) throw new ArgumentException("Invalid rect arguments");
+                        if (int.TryParse(sizes[0], out int width) && int.TryParse(sizes[1], out int height))
+                        {
+                            Rectangle rect = new Rectangle(currentPos.X, currentPos.Y, width, height);
+                            g.DrawRectangle(currentPen, rect);
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Invalid rect size");
+                        }
+                        break;
+                    }
+                case "write":
+                    {
+                        // write text on canvas at currentPos
+                        string text = args;
+                        using (Font font = new Font("Arial", 14))
+                        using (Brush brush = new SolidBrush(currentPen.Color))
+                        {
+                            g.DrawString(text, font, brush, currentPos);
+                        }
+                        break;
+                    }
+                default:
+                    // Unknown command
+                    throw new ArgumentException($"Unknown command '{cmd}'");
+            }
         }
     }
 }
